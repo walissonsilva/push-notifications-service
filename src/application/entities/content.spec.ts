90fe861453e46ab5bd7091118db0f65c18f0bfad
Walisson Silva <walissonsilva10@gmail.com>
2022-12-17 00:34:17 -0300

    test: add tests to content class

---
+++ src/application/entities/content.spec.ts
@@ -0,0 +1,17 @@
+import { Content } from './content';
+
+describe('Content', () => {
+  it('should be able to create a notification content with a valid length', () => {
+    const content = new Content('banana');
+
+    expect(content).toBeTruthy();
+  });
+
+  it('should NOT be able to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('abc')).toThrow(`Content length error`);
+  });
+
+  it('should NOT be able to create a notification content with more than than 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow(`Content length error`);
+  });
+});
