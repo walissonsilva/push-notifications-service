7d73b7a7b164b2be62acf8a630afee13567fc5a8
Walisson Silva <walissonsilva10@gmail.com>
2022-12-26 18:25:42 -0300

    test: add tests to Notification entity

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,3 +1,4 @@
+import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
 
 interface NotificationProps {
@@ -11,8 +12,11 @@ interface NotificationProps {
 export class Notification {
   private props: NotificationProps;
 
-  constructor(props: NotificationProps) {
-    this.props = props;
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
   }
 
   public set recipientId(recipientId: string) {
